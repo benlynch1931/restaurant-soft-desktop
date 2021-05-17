ece34f831769bc3212332902b9c307a99de7bd92
Ben <ben.lynch1931@gmail.com>
2021-05-17 14:39:00 +0100

    remove inline styling for menu rows

--- src/screens/MainMenu.js
+++ src/screens/MainMenu.js
@@ -6,11 +6,11 @@ const MainMenu = () => {
   
   return (
     <div>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div className="menu-rows">
         <button className="menu-buttons"><h2>Drinks</h2></button>
         <button className="menu-buttons"><h2>Food</h2></button>
       </div>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div className="menu-rows">
         <button className="menu-buttons"><h2>Settings</h2></button>
         <button className="menu-buttons"><h2>PLU</h2></button>
       </div>
