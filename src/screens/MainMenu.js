6cb162af041601531f878380bd00cef5e7c173f7
Ben <ben.lynch1931@gmail.com>
2021-05-19 15:28:50 +0100

    add onCLick to other menu options...
    only links to the main screen so appears to do nothing

--- src/screens/MainMenu.js
+++ src/screens/MainMenu.js
@@ -10,11 +10,11 @@ const MainMenu = () => {
   return (
     <div>
       <div className="menu-rows">
-        <button className="menu-buttons"><h2>Drinks</h2></button>
-        <button className="menu-buttons"><h2>Food</h2></button>
+        <button className="menu-buttons" onClick={() => { setScreen('main') }}><h2>Drinks</h2></button>
+        <button className="menu-buttons" onClick={() => { setScreen('main') }}><h2>Food</h2></button>
       </div>
       <div className="menu-rows">
-        <button className="menu-buttons"><h2>Settings</h2></button>
+        <button className="menu-buttons" onClick={() => { setScreen('main') }}><h2>Settings</h2></button>
         <button className="menu-buttons" onClick={() => { setScreen('plu') }}><h2>PLU</h2></button>
       </div>
     </div>
