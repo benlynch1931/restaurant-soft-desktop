475ff7c0a7b4eb7d7d08a05b43c4621d26b814bb
Ben <ben.lynch1931@gmail.com>
2021-05-20 11:06:52 +0100

    add settings screen for controller to context

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -4,12 +4,18 @@ export const AppContext = createContext();
 
 class AppContextProvider extends Component {
   state = {
-    screen: 'main'
+    screen: 'main',
+    settingsScreen: 'main'
 }
 
   setScreen = (newScreen) => {
     this.setState({ screen: newScreen})
   }
+  
+  setSettingsScreen = (newScreen) => {
+    this.setState({ screen: newScreen})
+  }
+
 
 
 
@@ -19,6 +25,7 @@ class AppContextProvider extends Component {
       <AppContext.Provider value={{
         ...this.state,
         setScreen: this.setScreen,
+        setSettingsScreen: this.setSettingsScreen
       }}>
       {this.props.children}
       </AppContext.Provider>
