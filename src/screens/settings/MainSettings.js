2b709dfeda8f8c2435f7679e2f2ce58f8dbf0f9a
Ben <ben.lynch1931@gmail.com>
2021-05-20 12:09:17 +0100

    update MainSettings file

--- src/screens/settings/MainSettings.js
+++ src/screens/settings/MainSettings.js
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 
-
+import { AppContext } from '../../contexts/AppContext.js'
+import '../../styles/MainSettings.css'
 
 const MainSettings = () => {
   
+  const { setSettingsScreen, setScreen } = useContext(AppContext);
+  
   return (
-    null
+    <div>
+      <div className="settings-menu-rows">
+        <button className="settings-menu-buttons" onClick={() => { setSettingsScreen('groups') }}><h2>Groups</h2></button>
+        <button className="settings-menu-buttons" onClick={() => { setSettingsScreen('departments') }}><h2>Departments</h2></button>
+      </div>
+      <div className="settings-menu-rows">
+        <button className="settings-menu-buttons" onClick={() => { setSettingsScreen('printing') }}><h2>Printing</h2></button>
+        <button className="settings-menu-buttons" onClick={() => { setSettingsScreen('stock') }}><h2>Stock</h2></button>
+      </div>
+      <div className="settings-menu-rows">
+        <button className="settings-menu-buttons" onClick={() => { setScreen('main') }}><h2>BACK</h2></button>
+        {/*<button className="settings-menu-buttons" onClick={() => { setScreen('main') }}><h2>BACK</h2></button>*/}
+      </div>
+    </div>
   )
 }
 
